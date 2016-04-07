295b0169df6090ccf0384bdbcc31f5609d8bd271
lxfontes <lxfontes@gmail.com>
2016-04-06 20:04:12 -0400

    more config params

--- main.go
+++ main.go
@@ -16,8 +16,12 @@ import (
 	"golang.org/x/oauth2"
 )
 
-type DoApiConfig struct {
+type Config struct {
 	AccessToken string
+	Domain      string
+	Ttl         uint32
+	PrivateIP   bool
+	BindAddress string
 }
 
 type DoApi struct {
@@ -26,14 +30,14 @@ type DoApi struct {
 	cachedDroplets map[string]godo.Droplet
 }
 
-func (cfg DoApiConfig) Token() (*oauth2.Token, error) {
+func (cfg Config) Token() (*oauth2.Token, error) {
 	token := &oauth2.Token{
 		AccessToken: cfg.AccessToken,
 	}
 	return token, nil
 }
 
-func NewDoApi(cfg DoApiConfig) *DoApi {
+func NewDoApi(cfg Config) *DoApi {
 	oauthClient := oauth2.NewClient(oauth2.NoContext, cfg)
 	r := &DoApi{
 		client: godo.NewClient(oauthClient),
@@ -98,11 +102,12 @@ func (api *DoApi) Droplets() ([]godo.Droplet, error) {
 
 func (api *DoApi) FilterCachedDroplets(pattern string) []godo.Droplet {
 	api.RLock()
-	defer api.RUnlock()
+	lookupDroplets := api.cachedDroplets
+	api.RUnlock()
 
 	r := []godo.Droplet{}
 
-	for name, droplet := range api.cachedDroplets {
+	for name, droplet := range lookupDroplets {
 		if strings.HasPrefix(name, pattern) {
 			r = append(r, droplet)
 		}
@@ -111,7 +116,7 @@ func (api *DoApi) FilterCachedDroplets(pattern string) []godo.Droplet {
 	return r
 }
 
-func fillResponse(queryAddress string, droplets []godo.Droplet, msg *dns.Msg) {
+func fillResponse(queryAddress string, droplets []godo.Droplet, cfg Config, msg *dns.Msg) {
 	for _, droplet := range droplets {
 		doIP, err := droplet.PublicIPv4()
 		if err != nil {
@@ -121,7 +126,7 @@ func fillResponse(queryAddress string, droplets []godo.Droplet, msg *dns.Msg) {
 
 		a := net.ParseIP(doIP)
 		rr := new(dns.A)
-		rr.Hdr = dns.RR_Header{Name: queryAddress, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0}
+		rr.Hdr = dns.RR_Header{Name: queryAddress, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: cfg.Ttl}
 		rr.A = a.To4()
 
 		msg.Answer = append(msg.Answer, rr)
@@ -130,9 +135,11 @@ func fillResponse(queryAddress string, droplets []godo.Droplet, msg *dns.Msg) {
 
 func main() {
 	var (
-		token       = flag.String("token", os.Getenv("DO_KEY"), "Your Personal Key (Also reads from $DO_KEY)")
-		domain      = flag.String("domain", "droplet-lb.", "Domain to respond queries for (note the dot at the end)")
-		bindAddress = flag.String("bind", "127.0.0.1:8053", "Expose DNS interface at this address")
+		token          = flag.String("token", os.Getenv("DO_KEY"), "Your Personal Key (Also reads from $DO_KEY)")
+		bindAddress    = flag.String("bind", "127.0.0.1:8053", "Expose DNS interface at this address")
+		domain         = flag.String("domain", "droplet-lb.", "Domain to respond queries for (note the dot at the end)")
+		ttl            = flag.Uint("ttl", 30, "ttl in seconds")
+		privateAddress = flag.Bool("private", false, "Use private IP instead of public")
 	)
 
 	flag.Parse()
@@ -142,8 +149,12 @@ func main() {
 		os.Exit(2)
 	}
 
-	cfg := DoApiConfig{
+	cfg := Config{
 		AccessToken: *token,
+		Domain:      *domain,
+		Ttl:         uint32(*ttl),
+		PrivateIP:   *privateAddress,
+		BindAddress: *bindAddress,
 	}
 
 	api := NewDoApi(cfg)
@@ -154,7 +165,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	dns.HandleFunc(*domain, func(w dns.ResponseWriter, r *dns.Msg) {
+	dns.HandleFunc(cfg.Domain, func(w dns.ResponseWriter, r *dns.Msg) {
 		if r.Question[0].Qtype != dns.TypeA {
 			log.Println("Invalid query type", r.Question[0].Qtype, ". 'A' ipv4 only.")
 			return
@@ -168,13 +179,13 @@ func main() {
 		m.Authoritative = true
 
 		droplets := api.FilterCachedDroplets(targetName)
-		fillResponse(r.Question[0].Name, droplets, m)
+		fillResponse(r.Question[0].Name, droplets, cfg, m)
 
 		w.WriteMsg(m)
 	})
 
 	go func() {
-		server := &dns.Server{Addr: *bindAddress, Net: "udp"}
+		server := &dns.Server{Addr: cfg.BindAddress, Net: "udp"}
 		err = server.ListenAndServe()
 		if err != nil {
 			log.Println("Failed to bring up DNS server")
